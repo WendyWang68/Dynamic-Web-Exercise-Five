31e1615baa3bf622fa3135ac683a9237d554cbb7
WendyWang68 <94095789+WendyWang68@users.noreply.github.com>
2023-11-09 08:16:32 -0500

    commit message

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express")
 const app = express();
+const firebase = require("firebase/app");
+// Port
 const port = 4000;
+// Our configuration information for firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCTDhv74vDBr62yblrSR_WHj6OMP-lNKXw",
+    authDomain: "exercise-five-web-2023.firebaseapp.com",
+    projectId: "exercise-five-web-2023",
+    storageBucket: "exercise-five-web-2023.appspot.com",
+    messagingSenderId: "1044444730636",
+    appId: "1:1044444730636:web:0679d67f0961c842dfa529",
+    measurementId: "G-GCXJ2GC71P"
+  };
+//Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require("./routes/index");
 //const singlePostRoute = require('./routes/singlePost');
 //const createPostRoute = require('./routes/createPost');
+
 
 app.use("/", indexRoute); 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express =require("express");
 const router = express.Router();
+// Initialize Firestore Database
+const firestore = require("firebase/firestore");
+// Create a reference to the database
+const db = firestore.getFirestore();
 
 //Define index route - get all posts
 router.get("/", (req, res) => {
-    res.send("All Posts!");
+    const postsQuery = firestore.getDocs(firestore.collection(db, "post"));
+    const postsArray = [];
+
+    postsQuery
+        .then((response) => {
+            response.forEach((post) => {
+                postsArray.push({ id: post.id, ...post.data()});
+            });
+            res.send(postsArray);
+            
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.send(error);
+        });
 });
 
 module.exports = router;
